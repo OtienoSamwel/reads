ea45bb8d59265c87e87ce46247f8b1f26e6656f9
OtienoSamwel <otienosamwel135@gmail.com>
2021-05-23 09:45:58 +0300

    create search destination

--- app/src/main/java/com/example/reads/MainActivity.kt
+++ app/src/main/java/com/example/reads/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.reads
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        //create a app bar configuration
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
     }
 }
