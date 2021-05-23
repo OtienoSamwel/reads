172c1b4ed75cf8b135843ed0fe6d21a8f1432a90
OtienoSamwel <otienosamwel135@gmail.com>
2021-05-23 10:33:09 +0300

    top toolbar

--- app/src/main/java/com/example/reads/MainActivity.kt
+++ app/src/main/java/com/example/reads/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.reads
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
