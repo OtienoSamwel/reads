bd97f5a57dd5fb1f950d3d5ef0a232dc18001ac0
OtienoSamwel <otienosamwel135@gmail.com>
2021-05-23 09:46:28 +0300

    incremental

--- app/src/main/java/com/example/reads/home/HomeFragment.kt
+++ app/src/main/java/com/example/reads/home/HomeFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.reads.R
 
 class HomeFragment : Fragment() {
+    private lateinit var button: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,5 +18,14 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button = view.findViewById(R.id.search_button)
+        button.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToSearchFragment()
+            view.findNavController().navigate(action)
+        }
     }
 }
