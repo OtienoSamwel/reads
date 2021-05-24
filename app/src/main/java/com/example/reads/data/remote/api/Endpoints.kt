854abaa244bc12baaf568716f170050d31c16dc7
OtienoSamwel <otienosamwel135@gmail.com>
2021-05-24 23:07:20 +0300

    edit endpoint to add search search results

--- app/src/main/java/com/example/reads/data/remote/api/Endpoints.kt
+++ app/src/main/java/com/example/reads/data/remote/api/Endpoints.kt
@@ -4,6 +4,6 @@ import com.example.reads.data.remote.model.Book
 import retrofit2.http.GET
 
 interface Endpoints {
-    @GET("/books/v1/volumes?q=harry")
+    @GET("/books/v1/volumes?maxResults=40&q=Jupiter's+Legacy")
     suspend fun getBooks(): Book
 }
