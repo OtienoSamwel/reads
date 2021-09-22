27b8410927c4c2d3b76de27c3452f06ed817f0a1
Otieno Samwel <otienosamwel135@gmail.com>
2021-09-22 12:39:14 +0300

    - replace gson with moshi

--- app/src/main/java/com/example/reads/data/remote/api/Api.kt
+++ app/src/main/java/com/example/reads/data/remote/api/Api.kt
@@ -5,18 +5,19 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object Api {
     private val retrofit: Retrofit = Retrofit.Builder()
-        .addConverterFactory(GsonConverterFactory.create())
         .baseUrl("https://www.googleapis.com")
+        .addConverterFactory(MoshiConverterFactory.create())
         .build()
 
     @Provides
+    @Singleton
     fun provideApiService(): Endpoints {
         return retrofit.create(Endpoints::class.java)
     }
