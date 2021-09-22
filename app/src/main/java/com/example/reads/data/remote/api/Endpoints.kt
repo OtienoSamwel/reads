bea3701537d85ce58d5333ef488ca6b29eb61f7f
Otieno Samwel <otienosamwel135@gmail.com>
2021-09-22 12:41:37 +0300

    - add call for genre specific data

--- app/src/main/java/com/example/reads/data/remote/api/Endpoints.kt
+++ app/src/main/java/com/example/reads/data/remote/api/Endpoints.kt
@@ -2,8 +2,12 @@ package com.example.reads.data.remote.api
 
 import com.example.reads.data.model.Book
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface Endpoints {
     @GET("/books/v1/volumes?maxResults=40&q=Harry+Potter")
     suspend fun getBooks(): Book
+
+    @GET("/books/v1/volumes?maxResults=40")
+    suspend fun getGenreBooks(@Query("q") q: String): Book
 }
