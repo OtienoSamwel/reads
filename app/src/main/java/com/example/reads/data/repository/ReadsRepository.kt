558a50680b11b83b32a6862eaf0a10c997b09f99
Otieno Samwel <otienosamwel135@gmail.com>
2021-09-22 12:46:05 +0300

    - implement functions

--- app/src/main/java/com/example/reads/data/repository/ReadsRepository.kt
+++ app/src/main/java/com/example/reads/data/repository/ReadsRepository.kt
@@ -2,8 +2,10 @@ package com.example.reads.data.repository
 
 import com.example.reads.data.model.Book
 import com.example.reads.data.model.Item
+import kotlinx.coroutines.flow.Flow
 
 interface ReadsRepository {
-    suspend fun getDbBooks(): List<Item>
-    suspend fun getBooks(): Book
+    val books: Flow<List<Item>>
+    suspend fun getBooks()
+    suspend fun getGenreBooks(genre: String): Book
 }
--- app/src/main/java/com/example/reads/data/repository/ReadsRepositoryImpl.kt
+++ app/src/main/java/com/example/reads/data/repository/ReadsRepositoryImpl.kt
@@ -4,19 +4,22 @@ import com.example.reads.data.local.ReadsDao
 import com.example.reads.data.model.Book
 import com.example.reads.data.model.Item
 import com.example.reads.data.remote.api.Endpoints
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ReadsRepositoryImpl @Inject constructor(
     private val service: Endpoints,
     private val dao: ReadsDao,
+) : ReadsRepository {
 
-    ) : ReadsRepository {
-    override suspend fun getDbBooks(): List<Item> {
-        return dao.getDbBooks()
+    override val books: Flow<List<Item>> = dao.getDbBooks()
+
+    override suspend fun getBooks() {
+        val book = service.getBooks()
+        dao.insertBooks(book.items)
     }
 
-    override suspend fun getBooks(): Book {
-        return service.getBooks()
-
+    override suspend fun getGenreBooks(genre: String): Book {
+        return service.getGenreBooks(genre)
     }
 }
