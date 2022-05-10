63e39699c2d7d9530947759b41b4ebb6fd66b3de
OtienoSamwel <otienosamwel135@gmail.com>
2022-05-10 12:29:20 +0300

    the main activity where all the navigation will occur

---
+++ app/src/main/java/com/otienosamwel/reads/ui/presentation/features/main/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.otienosamwel.reads.ui.presentation.features.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material.icons.rounded.Favorite
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.otienosamwel.reads.ui.presentation.features.home.Home
+import com.otienosamwel.reads.ui.theme.ReadsTheme
+import com.otienosamwel.reads.utils.ClearRippleTheme
+
+class MainActivity : ComponentActivity() {
+    companion object {
+        private val TAG = MainActivity::class.simpleName
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ReadsTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) { Navigation() }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = { BottomNav(navController) })
+    {
+        NavHost(navController, startDestination = Screens.Home.route) {
+            composable(route = Screens.Home.route) { Home(navController = navController) }
+            composable(route = Screens.Feed.route) {}
+            composable(route = Screens.Account.route) {}
+        }
+    }
+}
+
+@Composable
+fun BottomNav(navController: NavController) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    CompositionLocalProvider(LocalRippleTheme provides ClearRippleTheme) {
+        NavigationBar(modifier = Modifier.fillMaxWidth()) {
+            bottomNavScreens.forEach { screen ->
+                NavigationBarItem(
+                    selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                    icon = { Icon(imageVector = screen.Icon!!, contentDescription = screen.name) },
+                    label = { Text(text = screen.name) },
+                    alwaysShowLabel = true,
+                    onClick = {
+                        navController.navigate(screen.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    }
+                )
+            }
+        }
+    }
+}
+
+sealed class Screens(val route: String, val name: String, val Icon: ImageVector?) {
+    object Home : Screens("home", "Home", Icons.Rounded.Home)
+    object Feed : Screens("feed", "Feed", Icons.Rounded.Favorite)
+    object Account : Screens("account", "Account", Icons.Rounded.AccountCircle)
+}
+
+val bottomNavScreens = listOf(Screens.Home, Screens.Feed, Screens.Account)
