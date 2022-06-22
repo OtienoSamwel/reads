994a6e5b5960ad224d60d63aad5bd64401bf8ce8
OtienoSamwel <otienosamwel135@gmail.com>
2022-06-22 12:58:28 +0300

    start to set up home screen

--- app/src/main/java/com/otienosamwel/reads/ui/presentation/features/home/Home.kt
+++ app/src/main/java/com/otienosamwel/reads/ui/presentation/features/home/Home.kt
@@ -1,32 +1,65 @@
 package com.otienosamwel.reads.ui.presentation.features.home
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.otienosamwel.reads.ui.presentation.components.Sizes
+import com.otienosamwel.reads.data.model.Book
+import com.otienosamwel.reads.ui.presentation.components.BookItem
+import com.otienosamwel.reads.ui.presentation.components.SpaceMedium
+import com.otienosamwel.reads.ui.presentation.components.SpaceSmall
 import com.otienosamwel.reads.ui.theme.ReadsTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home(navController: NavController?) {
-    Column(
+
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarScrollState())
+
+
+    Scaffold(
         modifier = Modifier
-            .padding(Sizes.small)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.Start
-    ) {
-        Text(text = "Home Alone", style = MaterialTheme.typography.headlineMedium)
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = { HomeTopAppBar(scrollBehavior) }) { scaffoldPaddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(scaffoldPaddingValues)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Section()
+            SpaceMedium()
+            Section()
+            SpaceMedium()
+            Section()
+            SpaceMedium()
+            Section()
+            SpaceMedium()
+            Section()
+        }
+
     }
+}
+
+@Composable
+fun HomeTopAppBar(scrollBehavior: TopAppBarScrollBehavior) {
+    SmallTopAppBar(
+        title = { Text(text = "Reads") },
+        modifier = Modifier.fillMaxWidth(),
+        scrollBehavior = scrollBehavior
+    )
 }
 
 @Composable
@@ -35,6 +68,38 @@ fun HomePreview() {
     ReadsTheme {
         Surface(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
             Home(navController = null)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Section() {
+    Column(
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth(), horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "Section Title",
+            modifier = Modifier.padding(start = 5.dp),
+            style = MaterialTheme.typography.labelLarge
+        )
+
+        SpaceSmall()
+
+        LazyRow(
+            contentPadding = PaddingValues(horizontal = 5.dp),
+            horizontalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            items(20) {
+                BookItem(
+                    book = Book(
+                        name = "The tombs of Atuan",
+                        author = "Patrick Rothfuss"
+                    ), onAuthorClick = {}, onCoverClick = {}
+                )
+            }
         }
     }
 }
